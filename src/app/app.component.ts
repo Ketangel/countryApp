0adeab806d035875cff6e6dd2dbf53d3883928f2
jose angel <jose.albelt1817@gmail.com>
2025-03-25 15:45:32 -0500

    footer, layouts correcion del themes de colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterPageComponent } from "./shared/components/footer-page/footer-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
