c8e343c78785fb2bffb31408d56cd5b13c6671d2
jose angel <jose.albelt1817@gmail.com>
2025-04-01 08:46:42 -0500

    debounce o buscador automatico

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -10,6 +10,20 @@ export class CountrySearchInputComponent {
 
   inpuTitle = input<string>();
   outSearch = output<string>();
+  inputValue = signal<string>('');
+
+
+  debounceEffect = effect( (onCleanup) =>{
+    const value = this.inputValue();
+    const timeout = setTimeout( () => {
+      this.outSearch.emit(value)
+    },500);
+
+    onCleanup( () => {
+      clearTimeout(timeout)
+    }) 
+
+  })
 
   onSearch(value:string){
     this.outSearch.emit(value)
